145ff3b7d7ed51fc1fe372bdda4d3e5677de4c07
chanakan pasalung <644230021@webmail.npru.ac.th>
2021-12-22 13:43:27 +0700

    BasicJavaCode

---
+++ JavaCodeFromTeacher-main/JavaCodeFromTeacher-main/Week01/InputData.java
@@ -0,0 +1,15 @@
+package Week01;
+
+import java.util.Scanner;
+
+public class InputData {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter name = ");
+        String name = sc.nextLine();
+
+        System.out.println("===> "+name);
+        sc.close();
+    }
+}
---
+++ JavaCodeFromTeacher-main/JavaCodeFromTeacher-main/Week17/Test01.java
@@ -0,0 +1,13 @@
+package Week17;
+
+public class Test01 {
+    static void myMethod() {
+        System.out.println("Hello Java");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
